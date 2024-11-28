423e5eece9e225a3dc4fc47d4a82cfb1e2ddacef
Rangga Prabaswara <rangga.pw25@gmail.com>
2024-11-28 14:50:50 +0700

    done cv get users pencari

--- resources/views/backend/profil/cetak-cv.blade.php
+++ resources/views/backend/profil/cetak-cv.blade.php
@@ -49,21 +49,48 @@
 
 <body>
     <div class="container">
-        <!-- Header -->
+        <!-- Header Section -->
         <section class="section">
             <h1>{{ $user->name ?? 'No Username' }} </h1>
             <p>{{ $user->email }} | {{ $user->whatsapp ?? 'No Phone Provided' }} |
                 {{ $user->pencari->alamat ?? 'No Address Provided' }}</p>
         </section>
 
-        <!-- Ringkasan Profil -->
-        @if ($user->profile_summary)
-            <section class="section">
-                <h2>Ringkasan Profil</h2>
-                <p>{{ $user->profile_summary }}</p>
-            </section>
-        @endif
+        <!-- Personal Information Section -->
+        <section class="section">
+            <h2>Informasi Pribadi</h2>
+            <p><strong>Nomor KTP:</strong> {{ $user->pencari->ktp ?? 'Tidak Ada KTP' }}</p>
+            <p><strong>Tempat Lahir:</strong> {{ $user->pencari->tempat_lahir ?? 'Tidak Ada Tempat Lahir' }}</p>
+            <p><strong>Tanggal Lahir:</strong>
+                {{ \Carbon\Carbon::parse($user->pencari->tanggal_lahir)->format('d F, Y') ?? 'Tidak Ada Tanggal Lahir' }}
+            </p>
+            <p><strong>Jenis Kelamin:</strong>
+                {{ $user->pencari->gender == 'L' ? 'Laki-laki' : ($user->pencari->gender == 'P' ? 'Perempuan' : 'Tidak Ada Jenis Kelamin') }}
+            </p>
+            <p><strong>Alamat:</strong> {{ $user->pencari->alamat ?? 'Tidak Ada Alamat' }}</p>
+            <p><strong>Kode Pos:</strong> {{ $user->pencari->kodepos ?? 'Tidak Ada Kode Pos' }}</p>
+            <p><strong>Status Perkawinan:</strong>
+                @php
+                    $maritalStatus = DB::table('naker_marital')
+                        ->where('id', $user->pencari->id_status_perkawinan)
+                        ->first();
+                @endphp
 
+                {{ $maritalStatus ? $maritalStatus->name : 'Tidak Ada Status Perkawinan' }}
+            </p>
+            <p><strong>Agama:</strong>
+                @php
+                    $agama = DB::table('naker_agama')
+                        ->where('id', $user->pencari->id_agama)
+                        ->first();
+                @endphp
+
+                {{ $agama ? $agama->name : 'Tidak Ada Agama' }}
+            </p>
+            <p><strong>Media Sosial:</strong> {{ $user->pencari->medsos ?? 'Tidak Ada Media Sosial' }}</p>
+        </section>
+
+        <!-- Pendidikan Section -->
         @if ($pendidikan->isNotEmpty())
             <section class="section">
                 <h2>Pendidikan</h2>
@@ -73,42 +100,41 @@
                             <p>
                                 <span class="bold">{{ $item->pendidikan_name ?? 'Tidak Ada Pendidikan' }}</span> -
                                 <span class="bold">{{ $item->nama_sekolah ?? 'Tidak Ada Nama Sekolah' }}</span>,
-                                {{ $item->jurusan_name ?? 'Tidak Ada Jurusan' }} | Tahun Lulus: {{ $item->lulus ?? 'Tidak Ada Tahun Lulus' }}
+                                {{ $item->jurusan_name ?? 'Tidak Ada Jurusan' }} | Tahun Lulus:
+                                {{ $item->lulus ?? 'Tidak Ada Tahun Lulus' }}
                             </p>
-                            {{-- <p class="content">{{ $item->alamat_sekolah ?? 'Tidak Ada Alamat Sekolah' }}</p> --}}
                         </li>
                     @endforeach
                 </ul>
             </section>
         @endif
 
-        <!-- Pengalaman Kerja -->
+        <!-- Pengalaman Kerja Section -->
         @if ($pengalaman->isNotEmpty())
             <section class="section">
                 <h2>Pengalaman Kerja</h2>
                 <ul>
                     @foreach ($pengalaman as $item)
                         <li>
-                            <p><span class="bold">{{ $item->nama_perusahaan ?? 'Tidak Ada Nama Perusahaan' }}</span> - {{ $item->jabatan ?? 'Tidak Ada Nama Jabatan' }} |
-                                {{ $item->mulai_tahun ?? '-' }} - {{ $item->berhenti_tahun ?? '-'}}</p>
-                            </p>
-                            {{-- <p>{{ $item->alamat_perusahaan ?? '-' }}</p> --}}
+                            <p><span class="bold">{{ $item->nama_perusahaan ?? 'Tidak Ada Nama Perusahaan' }}</span>
+                                -
+                                {{ $item->jabatan ?? 'Tidak Ada Nama Jabatan' }} |
+                                {{ $item->mulai_tahun ?? '-' }} - {{ $item->berhenti_tahun ?? '-' }}</p>
                         </li>
                     @endforeach
                 </ul>
             </section>
         @endif
 
-        <!-- Keterampilan -->
+        <!-- Keterampilan Section -->
         @if ($keterampilan->isNotEmpty())
             <section class="section">
                 <h2>Keterampilan</h2>
                 <ul>
                     @foreach ($keterampilan as $item)
                         <li>
-                            <p>{{ $item->lembaga_penyelenggara ?? '-' }} - {{ $item->no_sertifikat ?? '-' }} - {{ $item->lulus_tahun ?? '-' }} | {{ $item->lembaga_penguji ?? '-' }}</p>
-                            {{-- <p>{{ $item->alamat_penyelenggara ?? '-' }}</p> --}}
-                            {{-- <p></p> --}}
+                            <p>{{ $item->lembaga_penyelenggara ?? '-' }} - {{ $item->no_sertifikat ?? '-' }} -
+                                {{ $item->lulus_tahun ?? '-' }} | {{ $item->lembaga_penguji ?? '-' }}</p>
                         </li>
                     @endforeach
                 </ul>
